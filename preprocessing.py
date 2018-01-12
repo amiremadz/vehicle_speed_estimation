fb31c65fced69448cad3af45136309ab144875d8
Amir <aemadzadeh@nvidia.com>
2018-01-12 15:28:23 -0800

    preprocessing done.

--- preprocessing.py
+++ preprocessing.py
@@ -3,8 +3,8 @@ import os
 import numpy as np
 
 from sklearn.utils import shuffle
-from utils import MyImage
-
+from utils import *
+ 
 
 gt_path  = "/home/aemadzadeh/Documents/comma.ai/data/"
 out_path = "/home/aemadzadeh/Documents/comma.ai/data/outputs"
@@ -19,17 +19,8 @@ channels = 3
 extensions = [".jpg"]
 file_names = [fn for fn in os.listdir(img_path) if any(fn.endswith(ext) for ext in extensions)]
 
-
-# create mask for splitting data
-num_imgs = len(file_names)
-
-num_train = int(0.9 * num_imgs)
-num_test  = num_imgs - num_train
-
-mask_train = np.arange(num_train)
-mask_test  = np.arange(num_train, num_imgs)
-
 # initialize image array which holds frames
+num_imgs = len(file_names)
 X = np.empty((num_imgs, height, width, channels), dtype='float32')
 
 print("\nConverting jpegs to numpy arrays...\n")
@@ -49,6 +40,46 @@ with open(os.path.join(gt_path, "train.txt")) as fh:
     y = fh.readlines()
 
 y = [float(val.strip()) for val in y]
+y = np.expand_dims(y, axis=1)
+
+###########
+num_imgs = len(y)
+X = X[:num_imgs]
+###########
+
+# create mask for splitting data
+num_train = int(0.9 * num_imgs)
+num_test  = num_imgs - num_train
+
+train_mask = np.arange(num_train)
+test_mask  = np.arange(num_train, num_imgs)
 
 
+# Shuffle
+print("\nShuffling the data...")
+X, y = shuffle(X, y, random_state=16)
 
+# Split data into train and test
+print("\nSplitting X into train and test...")
+X_train = X[train_mask]
+X_test  = X[test_mask]
+y_train = y[train_mask]
+y_test  = y[test_mask]
+
+
+print("\nWriting X-train to HDF5...")
+hdf5_manip = MyHDF5()
+
+hdf5_manip.outfile = os.path.join(out_path, "X_train.hdf5")
+hdf5_manip.write(X_train)
+
+hdf5_manip.outfile = os.path.join(out_path, "X_test.hdf5")
+hdf5_manip.write(X_test)
+
+hdf5_manip.outfile = os.path.join(out_path, "y_train.hdf5")
+hdf5_manip.write(y_train)
+
+hdf5_manip.outfile = os.path.join(out_path, "y_test.hdf5")
+hdf5_manip.write(y_test)
+
+print("\n Done!")
