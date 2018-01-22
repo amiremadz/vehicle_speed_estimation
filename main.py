d58a2af7c153f92e8d04bdcd51806e7c1ae299dd
Amir <aemadzadeh@nvidia.com>
2018-01-21 20:19:55 -0800

    Main: clean-up. Code for predicting from test.mp4.

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ from keras.applications.resnet50 import ResNet50
 hdf5_path = "/home/aemadzadeh/Documents/comma.ai/data/outputs"
 pickle_path = hdf5_path
 
+test_img_path = "/home/aemadzadeh/Documents/comma.ai/data/test_results/frames"
+test_out_path = "/home/aemadzadeh/Documents/comma.ai/data/test_results/outputs"
+
 def main():
     #load data
-    #dnn = VGG16
-    dnn = ResNet50
+    dnn = VGG16
+    #dnn = ResNet50
     #dnn = VGG19
     
-    
     X_train, y_train, X_test, y_test = load_data(hdf5_path)
-    
+   
+    # if features are not obtained yet
     #get_features(hdf5_path, dnn, 'block2_pool');
 
     print("Loading feature from HDF5 files...")
@@ -36,30 +39,108 @@ def main():
     hdf5_manip.hfile = os.path.join(hdf5_path, "y_test.hdf5")
     y_test = hdf5_manip.load()
 
-    print("Training linear regression model...")
-    clf = Ridge(alpha=5.0)
-    tic = time.time()
-    clf.fit(train_features, y_train)
-    toc = time.time()
-    print("Time elapsed: {0} seconds".format(toc - tic))
+    train_model = False
+    alpha = 15.0
 
-    #print("Predicting...")
-    #predictions = clf.predict(test_features)
+    if train_model:
+        print("Training linear regression model...")
+        clf = Ridge(alpha=alpha)
+        tic = time.time()
+        clf.fit(train_features, y_train)
+        toc = time.time()
+        print("Time elapsed: {0} seconds".format(toc - tic))
 
-    #mse = metrics.mean_squared_error(y_test, predictions)
-    #print("MSE: {0}".format(mse))
+        print("Predicting...")
+        predictions = clf.predict(test_features)
 
-    # Store the model in a pickle file
+        mse = metrics.mean_squared_error(y_test, predictions)
+        print("MSE: {0}".format(mse))
+
     pickle_manip = MyPickle()
-    pickle_manip.pfile = os.path.join(pickle_path, dnn.__name__, "clf_all.sav")
-    
-    #pickle_manip.dump(clf)
+    pickle_manip.pfile = os.path.join(pickle_path, dnn.__name__, "clf_all_" + str(alpha)  + ".sav")
+
+    if train_model:
+        # Store the model in a pickle file
+        pickle_manip.dump(clf)
 
     clf = pickle_manip.load()
-    predictions = clf.predict(test_features)
+    print("\nRidge model: ")
+    print(clf.get_params())
+    print(clf.score(train_features, y_train))
 
+    print("\nModel on Train data:")
+    pred = clf.predict(train_features)
+    print(pred)
+    print(metrics.mean_squared_error(y_train, pred))
+
+    print("\nModel on Test data:")
+    predictions = clf.predict(test_features)
+    print(predictions)
     mse = metrics.mean_squared_error(y_test, predictions)
-    print("MSE: {0}".format(mse))
+    print("\nMSE: {0}".format(mse))
+
+    #### Test results
+    
+    extract_features = False
+
+    if extract_features:
+        # image parameters
+        width  = 50
+        height = 50
+        channels = 3
+
+        # grab all filenames
+        extensions = [".jpg"]
+        file_names = [fn for fn in os.listdir(test_img_path) if any(fn.endswith(ext) for ext in extensions)]
+
+        # initialize image array which holds frames
+        num_imgs = len(file_names)
+        X = np.empty((num_imgs, height, width, channels), dtype='float32')
+
+        print("\nConverting jpegs to numpy arrays...\n")
+
+        img_size = (height, width)
+
+        for idx in range(num_imgs):
+            if idx % 1000 == 0:
+                print("Converting image {0}".format(file_names[idx]))
+            file_path = os.path.join(test_img_path, file_names[idx])
+            img_manip = MyImage(file_path, img_size)
+            img = img_manip.conv_jpg2array()
+            X[idx] = img
+
+        print("\nWriting X to HDF5...")
+        hdf5_manip = MyHDF5()
+
+        hdf5_manip.hfile = os.path.join(test_out_path, "X.hdf5")
+        hdf5_manip.write(X)
+
+        base_model = dnn(weights='imagenet', include_top=False)
+
+        model = Model(input=base_model.input, output=base_model.get_layer('block2_pool').output)
+
+        print("Computing X features...")
+        X_features = model.predict(X)
+        print("X features shape, before reshape: {0}".format(X_features.shape))
+        X_features = np.reshape(X_features, (X_features.shape[0], -1))
+        print("X features shape, after reshape: {0}".format(X_features.shape))
+ 
+        print("Writing X feature to HDF5 files...")
+        hdf5_manip = MyHDF5()
+
+        hdf5_manip.hfile = os.path.join(test_out_path, dnn.__name__, "X_features.hdf5")
+        hdf5_manip.write(X_features)
+    else:
+        print("Loading X feature from HDF5 files...")
+        hdf5_manip = MyHDF5()
+    
+        hdf5_manip.hfile = os.path.join(test_out_path, dnn.__name__, "X_features.hdf5")
+        X_features = hdf5_manip.load()
+
+    print(X_features.shape)
+    predictions = clf.predict(X_features)
+    print(predictions) 
+
 
 if __name__ == "__main__":
     main()
